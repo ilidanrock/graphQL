892072e88e517b37d2d42ff123416b3e7e1bef4e
Luis <rluis747@gmail.com>
2022-05-11 19:31:57 -0500

    Custom types

--- index.js
+++ index.js
@@ -1,48 +1,32 @@
 "use strict";
 
-const { graphql, buildSchema } = require("graphql");
-const express = require("express")
-var { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require('./lib/resolvers')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// definiendo el esquema
+
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
 
 
-const app = express()
-const port = process.env.PORT || 3000
 
-//definiendo el esquema
-
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`);
-
-//Configurar los resolvers
-
-const resolvers = {
-  hello: () => {
-    return "Hello Mom";
-  },
-};
-// Ejecutar el Schema
-
-graphql({
-  schema: schema,
-  source: "{hello}",
-  rootValue: resolvers,
-})
-  .then((data) => {
-    console.log(data);
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
   })
-  .catch((e) => {
-    console.log(e);
-  });
+);
 
-app.use('/api', graphqlHTTP({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: true
-}))
-
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}/api`);
-})
+});
