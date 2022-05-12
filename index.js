81846017bd88f5f53164549058630a11723d57b1
Luis <rluis747@gmail.com>
2022-05-11 20:18:37 -0500

    Resolvers with arguments

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const { buildSchema } = require("graphql");
+const { makeExecutableSchema } = require("graphql-tools");
 const express = require("express");
+var morgan = require('morgan')
 const { graphqlHTTP } = require("express-graphql");
 const { readFileSync } = require("fs");
 const { join } = require("path");
-const resolvers = require('./lib/resolvers')
+const resolvers = require("./lib/resolvers");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// definiendo el esquema
-
-const schema = buildSchema(
-  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+const typeDefs = readFileSync(
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8"
 );
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(
   "/api",
   graphqlHTTP({
